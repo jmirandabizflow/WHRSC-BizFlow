84eb9ea8bfdde7f6550fe57ee48d88a202580035
prabhjyot.virdi <prabhjyot.virdi@HHSL9YR8PL2.itsc.hhs-itsc.local>
2018-10-15 09:58:49 -0400

    Adding and updating files for creating a war file

--- scms/src/main/java/gov/hhs/induction/InductionClientApplication.java
+++ scms/src/main/java/gov/hhs/induction/InductionClientApplication.java
@@ -2,9 +2,17 @@ package gov.hhs.induction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class InductionClientApplication {
+public class InductionClientApplication extends SpringBootServletInitializer  {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(InductionClientApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(InductionClientApplication.class, args);
